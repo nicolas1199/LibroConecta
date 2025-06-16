4360191ccf9cd388dc1287a08687cb5af168ddd3
Nico <nico_opazo@outlook.cl>
2025-06-16 13:01:33 -0400

    Replace TypeORM with Sequelize for database connection and configuration

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -1,24 +1,29 @@
 "use strict";
-import { DataSource } from "typeorm";
+import { Sequelize } from "sequelize";
 import { DB_CONFIG } from "./configEnv.js"; // Importa el objeto DB_CONFIG
 
-// Configuración de la conexión a la base de datos usando TypeORM
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: DB_CONFIG.host,
-  port: DB_CONFIG.port,
-  username: DB_CONFIG.user,
-  password: DB_CONFIG.password,
-  database: DB_CONFIG.database,
-  entities: ["src/entity/**/*.js"], // o "dist/entity/**/*.js" si usas compilación
-  synchronize: true, // cuidado en producción
-  logging: false,
-});
+// Configuración de la conexión a la base de datos usando Sequelize
+const sequelize = new Sequelize(
+  DB_CONFIG.database,
+  DB_CONFIG.user,
+  DB_CONFIG.password,
+
+  {
+    host: DB_CONFIG.host,
+    port: DB_CONFIG.port,
+    dialect: "postgres",
+    logging: console.log,
+  }
+);
 
 // Función para inicializar la conexión a la base de datos
 export async function connectDB() {
   try {
-    await AppDataSource.initialize();
+    // Intenta autenticar la conexión
+    await sequelize.authenticate();
+    // Si la autenticación es exitosa, sincroniza los modelos
+    await sequelize.sync();
+    // Mensaje de éxito
     console.log("=> Conexión exitosa a la base de datos!");
   } catch (error) {
     console.error("Error al conectar con la base de datos:", error);
